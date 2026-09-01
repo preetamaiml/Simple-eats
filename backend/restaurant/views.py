f144f71757419802ad950e634432c83f70b821f2
preetamaiml <preetam.aiml@gmail.com>
2026-09-01 19:17:08 +0000

    ss

--- backend/restaurant/views.py
+++ backend/restaurant/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import MenuItem
 
-# Create your views here.
+
+def menu_list(request):
+
+    menu_items = MenuItem.objects.filter(
+        available=True
+    )
+
+    data = []
+
+    for item in menu_items:
+
+        data.append({
+            "id": item.id,
+            "name": item.name,
+            "description": item.description,
+            "price": float(item.price),
+            "category": item.category,
+            "available": item.available,
+            "isSpecial": item.is_special,
+            "prepTime": item.prep_time,
+            "rating": float(item.rating),
+            "reviews": item.reviews,
+        })
+
+    return JsonResponse(data, safe=False)
